cee7533b73f283c6c37b3295a8c1cd8f44ded0c2
Jonathan50390 <jonathan.guillerm@gmail.com>
2026-02-07 23:30:03 +0100

    EntityImageService.php

--- imageresize/src/Service/EntityImageService.php
+++ imageresize/src/Service/EntityImageService.php
@@ -1,15 +1,22 @@
 <?php
+if (!defined('_PS_VERSION_')) { exit; }
+
 class EntityImageService
 {
     private $processor;
-    public function __construct(ImageProcessorService $processor) { $this->processor = $processor; }
+
+    public function __construct(ImageProcessorService $processor)
+    {
+        $this->processor = $processor;
+    }
 
     public function regenerateImagesByEntity($entity)
     {
         switch ($entity) {
             case 'products': return $this->handleProducts();
             case 'categories': return $this->handleCategories();
-            case 'slides': return $this->processor->processSlideImage(null, null); // Logique simplifiée
+            case 'manufacturers': return $this->handleManufacturers();
+            case 'slides': return $this->handleSlides();
             default: return 0;
         }
     }
@@ -32,6 +39,29 @@ class EntityImageService
         $count = 0;
         foreach ($categories as $cat) {
             if ($this->processor->processCategoryImage($cat['id_category'], $types)) $count++;
+        }
+        return $count;
+    }
+
+    private function handleManufacturers()
+    {
+        $types = ImageType::getImagesTypes('manufacturers');
+        $manufacturers = Manufacturer::getManufacturers();
+        $count = 0;
+        foreach ($manufacturers as $m) {
+            if ($this->processor->processManufacturerImage($m['id_manufacturer'], $types)) $count++;
+        }
+        return $count;
+    }
+
+    private function handleSlides()
+    {
+        $count = 0;
+        $slides = Db::getInstance()->executeS('SELECT image FROM ' . _DB_PREFIX_ . 'homeslider_slides_lang');
+        if ($slides) {
+            foreach ($slides as $s) {
+                if (!empty($s['image']) && $this->processor->processSlideImage(null, $s['image'])) $count++;
+            }
         }
         return $count;
     }
